2aee74b1719f99eb6b7c54d8eaf417d49092807d
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-03-07 15:07:18 +0300

    ch_5 Authentication Google

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = "django-insecure-pqo6^uo1j*-*9sg8=b5r9emykh$*cerhhick+kofglsx%8k"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -26,6 +26,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "social_django",
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
@@ -125,6 +128,25 @@ LOGOUT_URL = 'logout'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 AUTHENTICATION_BACKENDS = [
-'django.contrib.auth.backends.ModelBackend',
-'account.authentication.EmailAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.twitter.TwitterOAuth',
+    'social_core.backends.google.GoogleOAuth2',
+]
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '143968824227-ps097trhm64re6gmtq8hskjib1guglck.apps.googleusercontent.com'  # Google Client ID
+# Google Client Secret
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
 ]
