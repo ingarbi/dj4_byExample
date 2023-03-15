054be7a3e1006bd17bc840d891c61e18db1c6cb1
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-03-15 10:26:31 +0300

    ch_13 Mixins & JS Drag&Drop

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -2,12 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
     path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("admin/", admin.site.urls),
+    path('course/', include('courses.urls')),
 ]
 
 if settings.DEBUG:
